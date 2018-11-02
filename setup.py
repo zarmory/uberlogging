72f140cdd7237611818566b28d95e3b19e920c6d
Zaar Hai <haizaar@haizaar.com>
2018-11-03 01:07:29 +1100

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("LICENSE") as f:
 
 setup(
     name="uberlogging",
-    version="0.0.1",
+    version="0.0.2",
     description="Highly opinionated logging configurator",
     long_description=readme,
     author="Zaar Hai",
